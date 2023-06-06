0a816806af7e6ce590888509a4b0a34bf7a78edc
abo-el30la <muhammadahmed1339@gmail.com>
2023-06-06 20:36:16 +0300

    start by model

---
+++ src/main/java/com/proprog/demo/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.proprog.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/sayhello")
+    public String sayHelloWorld() {
+        return "Hello World! ......";
+    }
+}
---
+++ src/main/java/com/proprog/demo/model/Todo.java
@@ -0,0 +1,41 @@
+package com.proprog.demo.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Todo {
+    @Id
+    private int id;
+    private String title;
+    private Boolean completed;
+
+
+    public Todo(int id, String title, Boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
